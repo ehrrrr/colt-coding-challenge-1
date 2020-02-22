4c1099cb43900dbb7aa58a65ec3dbee02b4dd970
ehrrrr <elisaveta.s.hristova@gmail.com>
2020-02-22 19:41:37 +0200

    Adding JS - moving through screens

--- src/script.js
+++ src/script.js
@@ -1,5 +1,38 @@
-// if (window.addEventListener) {
-// 	window.addEventListener('load', () => alert('Hello'), false); //W3C
-// } else {
-// 	window.attachEvent('onload', () => alert('Hello')); //IE
-// }
+class StepperForm {
+	constructor() {
+		this.nextBtn = document.querySelector('#btn-next');
+		this.prevBtn = document.querySelector('#btn-prev');
+
+		this.formScreens = Array.from(document.querySelectorAll('.form-screen'));
+		this.circles = Array.from(document.querySelectorAll('.circle'));
+		this.screenCounter = 0;
+
+		this.nextBtn.addEventListener('click', () => {
+			this.next();
+		});
+
+		this.prevBtn.addEventListener('click', () => {
+			this.prev();
+		});
+	}
+
+	next() {
+		this.toggleActive();
+		this.screenCounter++;
+		this.toggleActive();
+		this.prevBtn.disabled = Boolean(!this.screenCounter);
+	}
+
+	prev() {
+		this.toggleActive();
+		this.screenCounter--;
+		this.toggleActive();
+	}
+
+	toggleActive() {
+		this.circles[this.screenCounter].classList.toggle('active-circle');
+		this.formScreens[this.screenCounter].classList.toggle('active-form-screen');
+	}
+}
+
+new StepperForm();
