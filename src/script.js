1fafe713b1a149f0fc35484ae629443a35ffe7f6
ehrrrr <elisaveta.s.hristova@gmail.com>
2020-02-23 21:24:56 +0200

    Adding validation

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 class StepperForm {
 	constructor() {
-		this.nextBtn = document.querySelector('#btn-next');
-		this.submitBtn = document.querySelector('#btn-submit');
-		this.prevBtn = document.querySelector('#btn-prev');
+		this.formContainer = document.getElementById('form-container');
+		this.nextBtn = this.formContainer.querySelector('#btn-next');
+		this.submitBtn = this.formContainer.querySelector('#btn-submit');
+		this.prevBtn = this.formContainer.querySelector('#btn-prev');
 
-		this.formScreens = Array.from(document.querySelectorAll('.form-screen'));
-		this.circles = Array.from(document.querySelectorAll('.circle'));
+		this.formScreens = Array.from(this.formContainer.querySelectorAll('.form-screen'));
+		this.circles = Array.from(this.formContainer.querySelectorAll('.circle'));
 		this.screenCounter = 0;
 
 		this.nextBtn.addEventListener('click', () => {
@@ -16,9 +17,12 @@ class StepperForm {
 			this.prev();
 		});
 
+		this.submitBtn.addEventListener('click', () => {
+			this.submit();
+		});
+
 		document.onkeydown = (e) => {
 			e = e || window.event;
-
 			// Right arrow key
 			if (e.keyCode == '39') {
 				if (this.screenCounter >= 0 && this.screenCounter < 3) {
@@ -31,26 +35,27 @@ class StepperForm {
 					this.prev();
 				}
 			}
-
 			// Submit on enter
-			if (e.keyCode == '13' && !this.submitBtn.disabled) alert('Thanks');
+			if (e.keyCode == '13' && !this.submitBtn.disabled) this.submit();
 		};
 	}
 
 	next() {
-		// Update screen state
-		this.toggleActive(); // Remove current active
-		this.screenCounter++; // Next screen number
-		this.toggleActive(); // Add new active
-		this.moveCircle('next'); // Move the active dot
+		if (this.validate()) {
+			// Update screen state
+			this.toggleActive(); // Remove current active
+			this.screenCounter++; // Next screen number
+			this.toggleActive(); // Add new active
+			this.moveCircle('next'); // Move the active dot
 
-		// Update buttons state
-		this.prevBtn.disabled = false;
-		if (this.screenCounter >= 3) {
-			this.nextBtn.disabled = true;
-			this.nextBtn.style.visibility = 'hidden';
-			this.submitBtn.style.visibility = 'visible';
-			this.submitBtn.disabled = false;
+			// Update buttons state
+			this.prevBtn.disabled = false;
+			if (this.screenCounter >= 3) {
+				this.nextBtn.disabled = true;
+				this.nextBtn.style.visibility = 'hidden';
+				this.submitBtn.style.visibility = 'visible';
+				this.submitBtn.disabled = false;
+			}
 		}
 	}
 
@@ -88,6 +93,37 @@ class StepperForm {
 				this.circles[this.screenCounter].classList.add('circle-go-right');
 				this.circles[this.screenCounter].classList.remove('circle-go-left');
 			}
+		}
+	}
+
+	validateEmail(input) {
+		const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		return input.value.match(mailFormat);
+	}
+
+	validate() {
+		const shownInputs = Array.from(this.formScreens[this.screenCounter].querySelectorAll('input'));
+		let validationFlag = false;
+		shownInputs.forEach((input) => {
+			if (!input.checkValidity()) {
+				if (this.formScreens[this.screenCounter].querySelectorAll('.message').length < shownInputs.length) {
+					const message = document.createElement('SPAN');
+					message.classList.add('message');
+					message.innerText = input.validationMessage;
+					this.formScreens[this.screenCounter].insertBefore(message, input);
+				}
+				validationFlag = false;
+			} else {
+				validationFlag = true;
+			}
+		});
+		return validationFlag;
+	}
+
+	submit() {
+		if (this.validate()) {
+			alert(`🎉Successful submit!🎉\nHave a nice day!☺️`);
+			this.formContainer.querySelector('form').submit();
 		}
 	}
 }
